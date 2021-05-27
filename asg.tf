e2d1ec3e7b820535d4a5e51e2cec5c27a8beab8f
mvsnogueira-dnx <marcus.nogueira@dnx.solutions>
2021-05-28 04:39:13 +1000

    Creating spot Instance

--- asg.tf
+++ asg.tf
@@ -4,18 +4,19 @@ resource "aws_autoscaling_group" "asg" {
   min_size            = 1
   max_size            = 1
   vpc_zone_identifier = list(var.instances_subnet_ids[count.index])
+mixed_instances_policy { 
 
-  launch_template {
-    name    = aws_launch_template.default.name
-    version = "$Latest"
-  }
-
-  instances_distribution {
-      spot_instance_pools                      = 3
+    launch_template {
+      launch_template_specification {
+        launch_template_name = aws_launch_template.default.name
+        version = "$Latest"
+      }
+    }
+    instances_distribution {
+    
       on_demand_base_capacity                  = var.on_demand_base_capacity
       on_demand_percentage_above_base_capacity = var.on_demand_percentage
     }
-
 
   tags = concat(
     [ for key, value in var.tags: { key: key, value: value, propagate_at_launch: true } ],
